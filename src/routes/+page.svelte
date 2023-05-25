9164479a8e285482f94d0f1590122ea1fa74d04a
Carlosiano <carlosbastiand01@gmail.com>
2023-05-25 18:16:11 +0700

    nambahin nim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,14 @@
 <main>
   <Navbar/>
   <div class="content">
-    <div class="main"><h1>Hello World!</h1></div>
+    <div class="main">
+        <h1>Hello World!</h1>
+        <ul>
+            <li>Mario Sebastian Carlosiano 20.11.3439</li>
+            <li>Siti Nadia Nurul Fatihah 20.11.3472</li>
+            <li>Marssel Nasran Lingkubi 20.11.3443</li>
+        </ul>
+    </div>
     <div class="coba"></div>
   </div>
 </main>
@@ -18,7 +25,9 @@
     border-bottom-right-radius: 20%;
     border-bottom-left-radius: 20%;
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
   }
   .coba {
     height: 100px;
