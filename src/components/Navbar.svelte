e0027fa66c280a0fdd031e9c945ef1ef48b19d3e
Carlosiano <carlosbastiand01@gmail.com>
2023-05-25 17:58:28 +0700

    initial commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,66 @@
+<script>
+
+</script>
+
+<nav>
+  <div class="logo">EduWeb</div>
+  <ul> 
+    <li>
+      <a href="#">Home</a>
+    </li>
+    <li>
+      <a href="#">Courses</a>
+    </li>
+    <li>
+      <a href="#">Blog</a>
+    </li>
+    <li>
+      <a href="#">About Us</a>
+    </li>
+    <li class="login">
+      <a href="#">Login</a>
+    </li>
+    <li class="sign-up">
+      <a href="#">Sign Up</a>
+    </li>
+  </ul>
+</nav>
+
+  <style>
+  nav {
+    background-color: #FFF2E1;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 100px;
+    padding-right: 100px;
+    height: 100px;
+    align-items: center;
+  }
+  nav ul {
+    list-style-type: none;
+    display: flex;
+    gap: 80px;
+    align-items: center;
+  }
+  nav ul li a {
+    text-decoration: none;
+    color: #252641;
+    font-size: 20px;
+  }
+  .login {
+    background-color: white;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    width: 100px;
+    text-align: center;
+    border-radius: 20px;
+  }
+  .sign-up {
+    background-color: #F48C06;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    width: 100px;
+    text-align: center;
+    border-radius: 20px;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "../app.css"
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<main>
+  <Navbar/>
+  <div class="content">
+    <div class="main">Hello World!</div>
+    <div class="coba"></div>
+  </div>
+</main>
+
+<style>
+  .main {
+    background-color: #FFF2E1;
+    height: 100vh;
+    border-bottom-right-radius: 20%;
+    border-bottom-left-radius: 20%;
+  }
+  .coba {
+    height: 100px;
+  }
+</style>
