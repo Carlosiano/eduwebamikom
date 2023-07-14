8b0fb715b2e1e85baae061b6ac16ba0e7da8837b
Carlosiano <carlosbastiand01@gmail.com>
2023-07-14 12:15:47 +0700

    tambah google analytics

--- src/lib/analytics.svelte
+++ src/lib/analytics.svelte
@@ -1,9 +1,9 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import { page } from '$app/stores'
 
   $: {
     if (typeof gtag !== 'undefined') {
-      gtag('config', 'G-NWKVKBZFNY', {
+      gtag('config', 'G-YFY0Y17HPK', {
         page_title: document.title,
         page_path: $page.url.pathname,
       })
@@ -14,7 +14,7 @@
 <svelte:head>
   <script
     async
-    src="https://www.googletagmanager.com/gtag/js?id=G-NWKVKBZFNY">
+    src="https://www.googletagmanager.com/gtag/js?id=G-YFY0Y17HPK">
   </script>
   <script>
     window.dataLayer = window.dataLayer || []
@@ -24,6 +24,6 @@
     }
 
     gtag('js', new Date())
-    gtag('config', 'G-NWKVKBZFNY')
+    gtag('config', 'G-YFY0Y17HPK')
   </script>
-</svelte:head>
+</svelte:head> -->
